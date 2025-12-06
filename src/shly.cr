827b0cb60fafe79096425c1cbc76cf3255174989
huok <77536370+dee-shar@users.noreply.github.com>
2025-12-07 01:43:44 +0300

    Create shly.cr

---
+++ src/shly.cr
@@ -0,0 +1,16 @@
+require "json"
+require "http/client"
+
+class Shly
+  def initialize()
+    @headers = HTTP::Headers {
+      "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36"
+    }
+    uri = URI.parse("https://shly.link")
+    @http_client = HTTP::Client.new(uri)
+  end
+
+  def shorten_url(url : String) : JSON::Any
+    JSON.parse(@http_client.get("/_/api/?app=aAMHu&url=#{url}", headers: @headers).body)
+  end
+end    
